efbe9b4213b2b9449567332d1098567b4ba5691c
Yen-Hsun, Huang <s011208@gmail.com>
2014-03-31 21:59:27 +0800

    avoid npe

--- com.yenhsun.colorfilter/src/com/yenhsun/colorfilter/ColorFilterPanelService.java
+++ com.yenhsun.colorfilter/src/com/yenhsun/colorfilter/ColorFilterPanelService.java
@@ -1,3 +1,4 @@
+
 package com.yenhsun.colorfilter;
 
 import android.app.PendingIntent;
@@ -18,165 +19,159 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 public class ColorFilterPanelService extends Service {
-	private WindowManager mWm;
-	private WindowManager.LayoutParams mWmParams;
-	private int mScreenWidth, mScreenHeight;
-	private View mColorFilterPanel;
-	private static final boolean DEBUG = MainActivity.DEBUG;
-	private static final String TAG = MainActivity.TAG;
+    private WindowManager mWm;
 
-	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    private WindowManager.LayoutParams mWmParams;
 
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			// TODO Auto-generated method stub
-			if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-				boolean enable = getSharedPreferences(
-						MainActivity.SHARF_FILE_NAME, 0).getBoolean(
-						MainActivity.SHARF_KEY_ENABLE_FILTER, false);
-				int filterColor = getSharedPreferences(
-						MainActivity.SHARF_FILE_NAME, 0).getInt(
-						MainActivity.SHARF_KEY_FILTER_COLOR,
-						MainActivity.DEFAULT_FILTER_COLOR);
-				if (enable) {
-					try {
-						mWm.removeView(mColorFilterPanel);
-					} catch (Exception e) {// ignore
-					}
-					mWm.addView(mColorFilterPanel, mWmParams);
-					mColorFilterPanel.setBackgroundColor(filterColor);
-				} else {
-					try {
-						mWm.removeView(mColorFilterPanel);
-					} catch (Exception e) {// ignore
-					}
-				}
-			}
-		}
-	};
+    private int mScreenWidth, mScreenHeight;
 
-	public ColorFilterPanelService() {
-	}
+    private View mColorFilterPanel;
 
-	public void onCreate() {
-		super.onCreate();
-		if (DEBUG)
-			Log.d(TAG, "oncreate");
-		mScreenWidth = getResources().getDisplayMetrics().widthPixels;
-		mScreenHeight = getResources().getDisplayMetrics().heightPixels;
-		mWm = (WindowManager) getSystemService("window");
-		mWmParams = getWindowManagerParamsSettings();
-		mColorFilterPanel = new View(this);
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(Intent.ACTION_SCREEN_ON);
-		this.registerReceiver(mReceiver, filter);
-	}
+    private static final boolean DEBUG = MainActivity.DEBUG;
 
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		super.onStartCommand(intent, flags, startId);
-		if (mWm != null && mColorFilterPanel != null && mWmParams != null) {
-			boolean enable = getSharedPreferences(MainActivity.SHARF_FILE_NAME,
-					0).getBoolean(MainActivity.SHARF_KEY_ENABLE_FILTER, false);
-			int filterColor = getSharedPreferences(
-					MainActivity.SHARF_FILE_NAME, 0).getInt(
-					MainActivity.SHARF_KEY_FILTER_COLOR,
-					MainActivity.DEFAULT_FILTER_COLOR);
-			if (intent.getExtras() != null) {
-				boolean callFromNotification = intent.getExtras().getBoolean(
-						MainActivity.CHANG_STATE_FROM_NOTIFICATION, false);
-				if (callFromNotification) {
-					enable = !enable;
-					getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0)
-							.edit()
-							.putBoolean(MainActivity.SHARF_KEY_ENABLE_FILTER,
-									enable).commit();
-					MainActivity.handleSimpleNotification(this);
-				}
-			}
-			if (enable) {
-				try {
-					mWm.removeView(mColorFilterPanel);
-				} catch (Exception e) {// ignore
-				}
-				mWm.addView(mColorFilterPanel, mWmParams);
-				mColorFilterPanel.setBackgroundColor(filterColor);
-			} else {
-				try {
-					mWm.removeView(mColorFilterPanel);
-				} catch (Exception e) {// ignore
-				}
-			}
-			Toast.makeText(
-					this,
-					enable ? R.string.toast_show_filter
-							: R.string.toast_hide_filter, Toast.LENGTH_SHORT)
-					.show();
-		}
-		SwithWidget.performUpdate(this);
-		return Service.START_STICKY;
-	}
+    private static final String TAG = MainActivity.TAG;
 
-	public void onDestroy() {
-		super.onDestroy();
-	}
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
 
-	public int getStatusBarHeight() {
-		int result = 0;
-		int resourceId = getResources().getIdentifier("status_bar_height",
-				"dimen", "android");
-		if (resourceId > 0) {
-			result = getResources().getDimensionPixelSize(resourceId);
-		}
-		return result;
-	}
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            if (intent == null)
+                return;
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                boolean enable = getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0).getBoolean(
+                        MainActivity.SHARF_KEY_ENABLE_FILTER, false);
+                int filterColor = getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0).getInt(
+                        MainActivity.SHARF_KEY_FILTER_COLOR, MainActivity.DEFAULT_FILTER_COLOR);
+                if (enable) {
+                    try {
+                        mWm.removeView(mColorFilterPanel);
+                    } catch (Exception e) {// ignore
+                    }
+                    mWm.addView(mColorFilterPanel, mWmParams);
+                    mColorFilterPanel.setBackgroundColor(filterColor);
+                } else {
+                    try {
+                        mWm.removeView(mColorFilterPanel);
+                    } catch (Exception e) {// ignore
+                    }
+                }
+            }
+        }
+    };
 
-	public int getNavigationBarHeight() {
-		int result = 0;
-		int resourceId = getResources().getIdentifier("navigation_bar_height",
-				"dimen", "android");
-		if (resourceId > 0) {
-			result = getResources().getDimensionPixelSize(resourceId);
-		}
-		return result;
-	}
+    public ColorFilterPanelService() {
+    }
 
-	public WindowManager.LayoutParams getWindowManagerParamsSettings() {
-		WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-		int statusBarHeight = getStatusBarHeight();
-		int deviceHeight = mScreenHeight + statusBarHeight
-				+ getNavigationBarHeight();
-		params.x = 0;
-		params.y = -statusBarHeight;
-		params.height = deviceHeight + 200;
-		params.width = WindowManager.LayoutParams.MATCH_PARENT;
-		params.type = WindowManager.LayoutParams.TYPE_PHONE;
-		params.format = PixelFormat.RGBA_8888;
-		params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-				| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-				| WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-				| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
-		params.gravity = Gravity.CENTER;
-		params.horizontalWeight = 0;
-		params.verticalWeight = 0;
-		params.windowAnimations = android.R.style.Animation_Toast;
-		params.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
+    public void onCreate() {
+        super.onCreate();
+        if (DEBUG)
+            Log.d(TAG, "oncreate");
+        mScreenWidth = getResources().getDisplayMetrics().widthPixels;
+        mScreenHeight = getResources().getDisplayMetrics().heightPixels;
+        mWm = (WindowManager)getSystemService("window");
+        mWmParams = getWindowManagerParamsSettings();
+        mColorFilterPanel = new View(this);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        this.registerReceiver(mReceiver, filter);
+    }
 
-		return params;
-	}
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        if (mWm != null && mColorFilterPanel != null && mWmParams != null) {
+            boolean enable = getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0).getBoolean(
+                    MainActivity.SHARF_KEY_ENABLE_FILTER, false);
+            int filterColor = getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0).getInt(
+                    MainActivity.SHARF_KEY_FILTER_COLOR, MainActivity.DEFAULT_FILTER_COLOR);
+            if (intent != null && intent.getExtras() != null) {
+                boolean callFromNotification = intent.getExtras().getBoolean(
+                        MainActivity.CHANG_STATE_FROM_NOTIFICATION, false);
+                if (callFromNotification) {
+                    enable = !enable;
+                    getSharedPreferences(MainActivity.SHARF_FILE_NAME, 0).edit()
+                            .putBoolean(MainActivity.SHARF_KEY_ENABLE_FILTER, enable).commit();
+                    MainActivity.handleSimpleNotification(this);
+                }
+            }
+            if (enable) {
+                try {
+                    mWm.removeView(mColorFilterPanel);
+                } catch (Exception e) {// ignore
+                }
+                mWm.addView(mColorFilterPanel, mWmParams);
+                mColorFilterPanel.setBackgroundColor(filterColor);
+            } else {
+                try {
+                    mWm.removeView(mColorFilterPanel);
+                } catch (Exception e) {// ignore
+                }
+            }
+            Toast.makeText(this, enable ? R.string.toast_show_filter : R.string.toast_hide_filter,
+                    Toast.LENGTH_SHORT).show();
+        }
+        SwithWidget.performUpdate(this);
+        return Service.START_STICKY;
+    }
 
-	private final IColorChanged.Stub mBinder = new IColorChanged.Stub() {
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
-		@Override
-		public void changeBackground(int color) throws RemoteException {
-			// TODO Auto-generated method stub
-			mColorFilterPanel.setBackgroundColor(color);
-		}
-	};
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return mBinder;
-	}
+    public int getNavigationBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public WindowManager.LayoutParams getWindowManagerParamsSettings() {
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        int statusBarHeight = getStatusBarHeight();
+        int deviceHeight = mScreenHeight + statusBarHeight + getNavigationBarHeight();
+        params.x = 0;
+        params.y = -statusBarHeight;
+        params.height = deviceHeight + 200;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        params.format = PixelFormat.RGBA_8888;
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
+        params.gravity = Gravity.CENTER;
+        params.horizontalWeight = 0;
+        params.verticalWeight = 0;
+        params.windowAnimations = android.R.style.Animation_Toast;
+        params.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
+
+        return params;
+    }
+
+    private final IColorChanged.Stub mBinder = new IColorChanged.Stub() {
+
+        @Override
+        public void changeBackground(int color) throws RemoteException {
+            // TODO Auto-generated method stub
+            mColorFilterPanel.setBackgroundColor(color);
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return mBinder;
+    }
 
 }
